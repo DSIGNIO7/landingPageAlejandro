402e87f7c5889fa5d562f501338bfe1baa2aa445
apacheco <dsigniografix@gmail.com>
2024-08-23 22:58:59 -0500

    finis proyect

---
+++ script.js
@@ -0,0 +1,17 @@
+let swiper = new Swiper('.mySwiper', {
+  slidesPerView: 1,
+  centeredSlides: true,
+  loop: true,
+  spaceBetween: 30,
+  grabCursor: true,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  breakpoints: {
+    991: {
+      slidesPerView: 3,
+      spaceBetween: 20,
+    }
+  },
+})
